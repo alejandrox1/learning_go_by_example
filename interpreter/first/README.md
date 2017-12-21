155c10182cf6d8a7b42d897a3a12dab9526d0707
alejandrox1 <alarcj137@gmail.com>
2017-12-21 11:26:26 -0500

    monkey lexer - first round

---
+++ interpreter/first/README.md
@@ -0,0 +1,4 @@
+# Monkey Lexer
+
+* It does not support unicode - see `lexer.go:isLetter()`.
+* It does not support floats, hex, octal, etc. - see `lexer.go:isDigit()`.
---
+++ interpreter/first/lexer/lexer.go
@@ -0,0 +1,129 @@
+package lexer
+
+import (
+	"fmt"
+
+	"github.com/alejandrox1/learning_go_by_example/interpreter/first/token"
+)
+
+
+type Lexer struct {
+	input        string
+	// Current char under examination.
+	ch           byte
+	// Current position in input (points to current char).
+	position     int
+	// Current reading position in input (after current char).
+	readPosition int
+}
+
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+
+	// Initialize lexer.
+	l.readChar()
+
+	return l
+}
+
+// Avoid whitespace, newline, tabs, characters from being parsed by
+// readIdentifier.
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\n' || l.ch == '\t' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// Give the next character and advance position in input.
+// Move Lexer ch to next character(0 if end of input), increment position,and
+// readPosition.
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 // ASCII code for "NUL" char
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+// Return a token given TokenType and literal input.
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// CHeck whether the given argument is a letter. This defines the valid set of 
+// characters for indeitifers.
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// CHeck whether the given argument is a digit.
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+// Read identifier: advance lexer's position until it encounters a non-letter
+// character.
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// Advance the lexer's position to get the input number.
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			fmt.Printf("Illegal character [%q]\n", l.ch)
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
